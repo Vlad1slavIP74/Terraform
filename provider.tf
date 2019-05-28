2a47348a66f5ed4765e657974f29673627cf03a7
Vlad1slav <svlad1999@ukr.net>
2019-05-28 13:44:02 +0300

    MyOWn configuration

--- provider.tf
+++ provider.tf
@@ -1,26 +1,7 @@
-#Provides a VPC resource
 
+# Define AWS as our provider
 provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "190.160.0.0/16"
-  insstance_tenancy = "default"
-
-  tags {
-      Name = "main"
-      Location = "Kiev"
-  }
-}
-
-resource "aws_subnet" "subnet1" {
-  vpc_id = "${aws_vpc.main.id}"
-  cidr_block = "190.160.1.0/24"
-
-  tags {
-      Name = "Subnet1"
-  }
+  region = "${var.aws_region}"
 }
 
 
