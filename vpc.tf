3214fcc66bdc86a2828be508d97b9b008432fc4e
Vlad1slav <svlad1999@ukr.net>
2019-05-28 14:09:28 +0300

    Final

--- vpc.tf
+++ vpc.tf
@@ -60,3 +60,67 @@ resource "aws_route_table_association" "web-public-rt" {
 }
 
 
+# Define seсurity group for public subnet
+
+resource "aws_security_group" "sgweb" {
+    name = "vpc_test_web"
+    description = "You can have incoming HTTP conection and SSH access"
+    
+    # http
+    
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    #https
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # tcp
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks =  ["0.0.0.0/0"]
+  }
+   vpc_id = "${aws_vpc.default.id}"
+
+   tags {
+       Name = "Web server security group"
+   }
+}
+
+# Define the security group for private subnet
+# port 3306 MySQL
+
+resource "aws_security_group" "sgdb" {
+    name = "sg_test_web"
+    description = "Allow traffic from public subnet"
+
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["${var.public_subnet_cidr}"]
+  }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.public_subnet_cidr}"]
+  }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "DB SG"
+    }
+}
